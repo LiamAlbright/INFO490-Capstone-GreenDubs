f7a4cd9ca69f7ecd70fa693070ccb635946ce3d7
LiamAlbright <albrightliam56@gmail.com>
2020-05-11 04:33:03 -0700

    tagging works

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# INFO490-Capstone-GreenDubs
+# INFO490-Capstone-GreenDubs Mobile
 Climate change and environmental issues are arguably the biggest challenges of our era. At GreenDubs We believe that the solution to the environmental crisis can be addressed through a sustained localized community action that involves all of Humanity. An important problematic gap we have identified is that the current platforms and technologies available to citizen science communities/organizations pose a significant challenge to the volunteers due to a lack of effective human-centered design principles. We aim to create a web-based platform that allows for easier data upload, curation, and overall citizen science experience.
 
 **Team members**
--- js/app.js
+++ js/app.js
@@ -1,86 +1,85 @@
 // Sign up at https://openweathermap.org/ to obtain an API key
 //import * as tf from '@tensorflow/tfjs';
 //import * as tf from '@tensorflow/tfjs-core';
-let inputMin = -1;
-let inputMax = 1;
-let normalizationConstant = (inputMax - inputMin) / 255.0;
-const IMAGE_CLASSES = ["cane","cavallo","elefante","farfalla","gallina","gatto","mucca","pecora","ragno","scoiattolo"];
+var track = document.getElementById("upload");
+track.addEventListener("change", handleFiles, false);
+var subBut = document.getElementById("submitImg");
+var  fileList;
+var imgEl = document.getElementById('img');
+var tagBox = document.getElementById("checkTagBox");
+// Create an unordered list
+var list = document.getElementById('headerList');
+let net;
 
-async function runmodel(){
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgToId = imgEl;
+  const result = await net.classify(imgToId);
+  console.log(result);
+  myFunction(result)
+}
+ //
+
+
+ function handleFiles() {
+  fileList = this.files;
+  console.log("Upload", fileList);
+  track.style.background = "url('https://cdn2.iconfinder.com/data/icons/font-awesome/1792/upload-512.png') center center no-repeat cornsilk";
+  track.style.backgroundSize = "40px";
+  if (fileList[0].type !== "image/png" && fileList[0].type !== "image/jpeg") {
+      alert("Please retry and upload a image file. Of type png or jpg!");
+      track.value = "";
+  } else if (fileList[0].size > 5100000) {
+      alert("Image file too big. We recommend to use mp3 file less than 5MB.");
+      track.value = "";
+  } else {
+      console.log("Accept");
+      track.style.background = "url('https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png')  center center no-repeat cornsilk";
+      track.style.backgroundSize = "40px";
+  }
+}
+
+subBut.addEventListener("click", function (e) {
+  e.preventDefault();
+    if (fileList == null) {
+        alert("Please upload a image before you try tagging");
+    } else {
+      console.log(fileList[0].name);
+      imgEl.src = URL.createObjectURL(fileList[0]);
+      app();
+    } 
+});
+
+function myFunction(res) {
+  if (res != null){
+    tagBox.style.display = "block";
+  } else {
+    tagBox.style.display = "none";
+  }
+  console.log(res);
 
 
 
 
- const model = await  tf.loadLayersModel('./keras_model/modeltfjs.json');
- const imgEl = document.getElementById('img');
- const exampleimg = tf.browser.fromPixels(imgEl); 
+// Create a fragement
+var fragment = document.createDocumentFragment();
 
- const normalized = exampleimg.toFloat().mul(normalizationConstant).add(inputMin);
+// Create a list item for each wizard
+// and append it to the fragment
+res.forEach(function (res) {
+	var li = document.createElement('li');
+	li.textContent = res.className;
+	fragment.appendChild(li);
+});
 
- const resized = tf.image.resizeBilinear(
-    exampleimg, [50, 50], true);
-
-  //  const batched = resized.reshape([-1, 50, 50, 1]);
-    const batched = resized.reshape([-1, 50, 50,3]);
-
- const prediction = model.predict(batched);
-
- //const classes = await getTopKClasses(prediction, 10);
- const softmax = prediction.softmax();
- const values = await softmax.data();
- console.log(values)
-
- //console.log(classes)
+// Append the fragement to the list
+list.appendChild(fragment);
 
 
 }
-
- runmodel();
-
-
-/////// CODE FROM TENSORFLOW JS MOBILE NET THAT IM PLAYING AROUND WITH
-// async function getTopKClasses(logits, topK) {
-//     const softmax = logits.softmax();
-//     const values = await softmax.data();
-//     softmax.dispose();
-
-//     const valuesAndIndices = [];
-//     for (let i = 0; i < values.length; i++) {
-//     valuesAndIndices.push({value: values[i], index: i});
-//     }
-//     valuesAndIndices.sort((a, b) => {
-//     return b.value - a.value;
-//     });
-//     const topkValues = new Float32Array(topK);
-//     const topkIndices = new Int32Array(topK);
-//     for (let i = 0; i < topK; i++) {
-//     topkValues[i] = valuesAndIndices[i].value;
-//     topkIndices[i] = valuesAndIndices[i].index;
-//     }
-
-//     const topClassesAndProbs = [];
-//     for (let i = 0; i < topkIndices.length; i++) {
-//     topClassesAndProbs.push({
-//     className: IMAGENET_CLASSES[topkIndices[i]],
-//     probability: topkValues[i]
-//     });
-//     }
-// return topClassesAndProbs;
-// }
-
-//  let net;
-
-// async function app() {
-//   console.log('Loading mobilenet..');
-
-//   // Load the model.
-//   net = await mobilenet.load();
-//   console.log('Successfully loaded model');
-
-//   // Make a prediction through the model on our image.
-//   const imgEl = document.getElementById('img');
-//   const result = await net.classify(imgEl);
-//   console.log(result);
-// }
-
-// app();
